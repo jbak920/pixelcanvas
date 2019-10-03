13676b4117e14872f45820bfc327f59b989e203c
Jonathan Baker <jbak920@gmail.com>
2019-10-03 01:04:40 +0100

    Making starting piece random

--- pixelcanvas.py
+++ pixelcanvas.py
@@ -18,7 +18,7 @@ CANVAS_HEIGHT  = 32      # Number of pixels per column
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 30     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 
