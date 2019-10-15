6da47787196598ef9583276dd2d1b75e934d4c3a
Jonathan Baker <jbak920@gmail.com>
2019-10-15 03:41:36 +0100

    Adding a metroid animation

--- pixeldriver.py
+++ pixeldriver.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
         elif ('snake' in args.program.lower()):
             print 'snek'
         elif('animation' in args.program.lower()):
-            animate(canvas, 'standing_mario')
+            animate(canvas, 'metroid')
         else:
             print "You didn't choose a program!"
 
